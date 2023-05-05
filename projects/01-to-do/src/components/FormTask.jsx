a0286ad05840fde163b0bd1395913e30fcb00c31
Andres Toro <atoro@intap.com.co>
2023-05-05 15:03:38 -0500

    Status field is added to the form

--- projects/01-to-do/src/components/FormTask.jsx
+++ projects/01-to-do/src/components/FormTask.jsx
@@ -1,16 +1,18 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ListTask } from './ListTask'
 import { useForm } from '../hooks/useForm'
+import { statusData } from '../helpers/statusData';
 
 export const FormTask = () => {
   const { valueForm, handleInputChange, resetForm } = useForm({
     name: '',
     description: '',
     date_start: '',
-    date_end: ''
+    date_end: '',
+    status: ''
   });
 
-  const { name, description, date_start, date_end } = valueForm;
+  const { name, description, date_start, date_end, status } = valueForm;
 
   const handleSubmitForm = (event) => {
     event.preventDefault();
@@ -47,23 +49,49 @@ export const FormTask = () => {
             {/* Form task */}
             <form onSubmit={ handleSubmitForm }>
               <fieldset className='mb-3'>
-                <label htmlFor="task" className='block font-medium mb-2'>Task</label>
-                <input type="text" id='task' name="name" value={ name } onChange={ handleInputChange } maxLength={ 30 } className='block bg-gray-50 border border-gray-300 p-2.5 rounded-md text-gray-900 text-sm w-full' placeholder='New task' />
+                <label htmlFor='task' className='block font-medium mb-2'>Task</label>
+                <input type='text' id='task' name='name' value={ name } onChange={ handleInputChange } maxLength={ 30 } className='block bg-gray-50 border border-gray-300 p-2.5 rounded-md text-gray-900 text-sm w-full' placeholder='New task' />
               </fieldset>
               <fieldset className='mb-3'>
-                <label htmlFor="description" className='block font-medium mb-2'>Description</label>
-                <textarea id="description" name="description" value={ description } onChange={ handleInputChange } className='block bg-gray-50 border border-gray-300 p-2.5 rounded-md text-gray-900 text-sm w-full' rows="5" placeholder='Decription'></textarea>
+                <label htmlFor='description' className='block font-medium mb-2'>Description</label>
+                <textarea id='description' name='description' value={ description } onChange={ handleInputChange } className='block bg-gray-50 border border-gray-300 p-2.5 rounded-md text-gray-900 text-sm w-full' rows='5' placeholder='Decription'></textarea>
               </fieldset>
-              <div className='grid grid-cols-2 gap-4'>
+              <div className='grid grid-cols-2 gap-4 mb-3'>
                 <fieldset className='mb-3'>
-                  <label htmlFor="date_start" className='block font-medium mb-2'>Date start</label>
-                  <input type="date" id='date_start' name='date_start' value={ date_start } onChange={ handleInputChange } className='block bg-gray-50 border border-gray-300 p-2.5 rounded-md text-gray-900 text-sm w-full' placeholder='New task' />
+                  <label htmlFor='date_start' className='block font-medium mb-2'>Date start</label>
+                  <input type='date' id='date_start' name='date_start' value={ date_start } onChange={ handleInputChange } className='block bg-gray-50 border border-gray-300 p-2.5 rounded-md text-gray-900 text-sm w-full' placeholder='New task' />
                 </fieldset>
                 <fieldset className='mb-3'>
-                  <label htmlFor="date_end" className='block font-medium mb-2'>Date end</label>
-                  <input type="date" id='date_end' name='date_end' value={ date_end } onChange={ handleInputChange } className='block bg-gray-50 border border-gray-300 p-2.5 rounded-md text-gray-900 text-sm w-full' placeholder='New task' />
+                  <label htmlFor='date_end' className='block font-medium mb-2'>Date end</label>
+                  <input type='date' id='date_end' name='date_end' value={ date_end } onChange={ handleInputChange } className='block bg-gray-50 border border-gray-300 p-2.5 rounded-md text-gray-900 text-sm w-full' placeholder='New task' />
                 </fieldset>
               </div>
+              {/* <div className='grid grid-cols-3 gap-4 mb-3'>
+                <fieldset>
+                  <input type='checkbox' id='status' className='w-4 h-4 bg-gray-100 border-gray-300 rounded' />
+                  <label htmlFor='status' className='ml-2 font-medium text-sm text-gray-600'>Pending</label>
+                </fieldset>
+                <fieldset>
+                  <input type='checkbox' id='status' className='w-4 h-4 bg-gray-100 border-gray-300 rounded' />
+                  <label htmlFor='status' className='ml-2 font-medium text-sm text-gray-600'>Proccess</label>
+                </fieldset>
+                <fieldset>
+                  <input type='checkbox' id='status' className='w-4 h-4 bg-gray-100 border-gray-300 rounded' />
+                  <label htmlFor='status' className='ml-2 font-medium text-sm text-gray-600'>Completed</label>
+                </fieldset>
+              </div> */}
+              <fieldset className='mb-3'>
+                <label htmlFor="status" className='block font-medium mb-2'>Status</label>
+                <select name="status" id="status" value={ status } onChange={ handleInputChange } className='block bg-gray-50 border border-gray-300 p-2.5 rounded-lg text-sm w-full'>
+                  {
+                    statusData.map(status => {
+                      return (
+                        <option key={ status.code } value={ status.code }>{ status.name }</option>
+                      )
+                    })
+                  }
+                </select>
+              </fieldset>
               <fieldset className='flex gap-2'>
                 <button type='submit' className='block bg-green-600 px-2 py-1 rounded-lg text-white font-medium hover:bg-green-700'>Save</button>
                 <button type='button' onClick={ () => resetForm() } className='block bg-gray-600 px-2 py-1 rounded-lg text-white font-medium hover:bg-gray-700'>Clear</button>
---
+++ projects/01-to-do/src/helpers/statusData.js
@@ -0,0 +1,5 @@
+export const statusData = [
+    {code: 1, name: 'Pending'},
+    {code: 2, name: 'Process'},
+    {code: 3, name: 'Completed'}
+]
