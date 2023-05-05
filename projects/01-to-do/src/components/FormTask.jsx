37d5a33d107e407a6857aa2f64cb6e77c98eb3f6
Andres Toro <atoro@intap.com.co>
2023-05-05 10:03:35 -0500

    Create task and save in localStorage

--- projects/01-to-do/src/components/FormTask.jsx
+++ projects/01-to-do/src/components/FormTask.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ListTask } from './ListTask'
+import { useForm } from '../hooks/useForm'
 
 export const FormTask = () => {
+  const { valueForm, handleInputChange, resetForm } = useForm({
+    name: '',
+    description: '',
+    date_start: '',
+    date_end: ''
+  });
+
+  const { name, description, date_start, date_end } = valueForm;
+
+  const handleSubmitForm = (event) => {
+    event.preventDefault();
+
+    if (!name.length) {
+      return
+    } 
+    
+    if (!description.length) {
+      return
+    }
+
+    if (Date.parse(date_start) > Date.parse(date_end)) {
+      return
+    }
+    
+    // Get tasks
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    tasks.push(valueForm)
+
+    // Save tasks
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+    
+    resetForm();
+  }
+  
   return (
     <>
       <div className='grid md:grid-cols-3 gap-6'>
@@ -10,28 +45,28 @@ export const FormTask = () => {
           <div className='bg-white border border-gray-200 rounded-lg p-6'>
             <h5 className='mb-4 font-bold text-2xl text-center'>Form Task</h5>
             {/* Form task */}
-            <form className='mb-3'>
+            <form onSubmit={ handleSubmitForm }>
               <fieldset className='mb-3'>
                 <label htmlFor="task" className='block font-medium mb-2'>Task</label>
-                <input type="text" id='task' className='block bg-gray-50 border border-gray-300 p-2.5 rounded-md text-gray-900 text-sm w-full' placeholder='New task' />
+                <input type="text" id='task' name="name" value={ name } onChange={ handleInputChange } maxLength={ 30 } className='block bg-gray-50 border border-gray-300 p-2.5 rounded-md text-gray-900 text-sm w-full' placeholder='New task' />
               </fieldset>
               <fieldset className='mb-3'>
                 <label htmlFor="description" className='block font-medium mb-2'>Description</label>
-                <textarea className='block bg-gray-50 border border-gray-300 p-2.5 rounded-md text-gray-900 text-sm w-full' name="" id="description" rows="5"></textarea>
+                <textarea id="description" name="description" value={ description } onChange={ handleInputChange } className='block bg-gray-50 border border-gray-300 p-2.5 rounded-md text-gray-900 text-sm w-full' rows="5" placeholder='Decription'></textarea>
               </fieldset>
               <div className='grid grid-cols-2 gap-4'>
                 <fieldset className='mb-3'>
                   <label htmlFor="date_start" className='block font-medium mb-2'>Date start</label>
-                  <input type="date" id='date_start' className='block bg-gray-50 border border-gray-300 p-2.5 rounded-md text-gray-900 text-sm w-full' placeholder='New task' />
+                  <input type="date" id='date_start' name='date_start' value={ date_start } onChange={ handleInputChange } className='block bg-gray-50 border border-gray-300 p-2.5 rounded-md text-gray-900 text-sm w-full' placeholder='New task' />
                 </fieldset>
                 <fieldset className='mb-3'>
                   <label htmlFor="date_end" className='block font-medium mb-2'>Date end</label>
-                  <input type="date" id='date_end' className='block bg-gray-50 border border-gray-300 p-2.5 rounded-md text-gray-900 text-sm w-full' placeholder='New task' />
+                  <input type="date" id='date_end' name='date_end' value={ date_end } onChange={ handleInputChange } className='block bg-gray-50 border border-gray-300 p-2.5 rounded-md text-gray-900 text-sm w-full' placeholder='New task' />
                 </fieldset>
               </div>
               <fieldset className='flex gap-2'>
                 <button type='submit' className='block bg-green-600 px-2 py-1 rounded-lg text-white font-medium hover:bg-green-700'>Save</button>
-                <button className='block bg-gray-600 px-2 py-1 rounded-lg text-white font-medium hover:bg-gray-700'>Clear</button>
+                <button type='button' onClick={ () => resetForm() } className='block bg-gray-600 px-2 py-1 rounded-lg text-white font-medium hover:bg-gray-700'>Clear</button>
               </fieldset>
             </form>
             {/* End form task */}
