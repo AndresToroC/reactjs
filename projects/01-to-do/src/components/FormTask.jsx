d48421b386b467141a99a0919e3c044dfdc36fa7
Andres Toro <atoro@intap.com.co>
2023-05-05 08:38:35 -0500

    View form and task lists

--- projects/01-to-do/src/components/FormTask.jsx
+++ projects/01-to-do/src/components/FormTask.jsx
@@ -4,32 +4,42 @@ import { ListTask } from './ListTask'
 export const FormTask = () => {
   return (
     <>
-      {/* Card tasks */}
-      <div className='bg-white border border-gray-200 rounded-lg p-6 lg:w-3/5'>
-        <h5 className='mb-4 font-bold text-2xl text-center'>Form Task</h5>
-        {/* Form task */}
-        <form className='mb-3'>
-          <fieldset className='mb-3'>
-            <label htmlFor="task" className='block font-medium mb-2'>Task</label>
-            <input type="text" id='task' className='block bg-gray-50 border border-gray-300 p-2.5 rounded-md text-gray-900 text-sm w-full' placeholder='New task' />
-          </fieldset>
-          <div className='grid grid-cols-2 gap-4'>
-            <fieldset className='mb-3'>
-              <label htmlFor="date_start" className='block font-medium mb-2'>Date start</label>
-              <input type="date" id='date_start' className='block bg-gray-50 border border-gray-300 p-2.5 rounded-md text-gray-900 text-sm w-full' placeholder='New task' />
-            </fieldset>
-            <fieldset className='mb-3'>
-              <label htmlFor="date_end" className='block font-medium mb-2'>Date end</label>
-              <input type="date" id='date_end' className='block bg-gray-50 border border-gray-300 p-2.5 rounded-md text-gray-900 text-sm w-full' placeholder='New task' />
-            </fieldset>
+      <div className='grid md:grid-cols-3 gap-6'>
+        <div className='md:col-span-1'>
+          {/* Card tasks */}
+          <div className='bg-white border border-gray-200 rounded-lg p-6'>
+            <h5 className='mb-4 font-bold text-2xl text-center'>Form Task</h5>
+            {/* Form task */}
+            <form className='mb-3'>
+              <fieldset className='mb-3'>
+                <label htmlFor="task" className='block font-medium mb-2'>Task</label>
+                <input type="text" id='task' className='block bg-gray-50 border border-gray-300 p-2.5 rounded-md text-gray-900 text-sm w-full' placeholder='New task' />
+              </fieldset>
+              <fieldset className='mb-3'>
+                <label htmlFor="description" className='block font-medium mb-2'>Description</label>
+                <textarea className='block bg-gray-50 border border-gray-300 p-2.5 rounded-md text-gray-900 text-sm w-full' name="" id="description" rows="5"></textarea>
+              </fieldset>
+              <div className='grid grid-cols-2 gap-4'>
+                <fieldset className='mb-3'>
+                  <label htmlFor="date_start" className='block font-medium mb-2'>Date start</label>
+                  <input type="date" id='date_start' className='block bg-gray-50 border border-gray-300 p-2.5 rounded-md text-gray-900 text-sm w-full' placeholder='New task' />
+                </fieldset>
+                <fieldset className='mb-3'>
+                  <label htmlFor="date_end" className='block font-medium mb-2'>Date end</label>
+                  <input type="date" id='date_end' className='block bg-gray-50 border border-gray-300 p-2.5 rounded-md text-gray-900 text-sm w-full' placeholder='New task' />
+                </fieldset>
+              </div>
+              <fieldset className='flex gap-2'>
+                <button type='submit' className='block bg-green-600 px-2 py-1 rounded-lg text-white font-medium hover:bg-green-700'>Save</button>
+                <button className='block bg-gray-600 px-2 py-1 rounded-lg text-white font-medium hover:bg-gray-700'>Clear</button>
+              </fieldset>
+            </form>
+            {/* End form task */}
           </div>
-          <fieldset className='flex gap-2'>
-            <button type='submit' className='block bg-green-600 px-2 py-1 rounded-lg text-white font-medium hover:bg-green-700'>Save</button>
-            <button className='block bg-gray-600 px-2 py-1 rounded-lg text-white font-medium hover:bg-gray-700'>Clear</button>
-          </fieldset>
-        </form>
-        {/* End form task */}
-        <ListTask />
+        </div>
+        <div className='md:col-span-2'>
+          <ListTask />
+        </div>
       </div>
     </>
   )
--- projects/01-to-do/src/components/ListTask.jsx
+++ projects/01-to-do/src/components/ListTask.jsx
@@ -3,47 +3,63 @@ import React from 'react'
 export const ListTask = () => {
   return (
     <>
-      <hr />
-      <div className='mt-5 h-80 overflow-y-auto'>
-        <h2 className='mb-4 font-bold text-lg text-center'>List Tasks</h2>
-        {/* <ul class="max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
-          <li>
-            At least 10 characters (and up to 100 characters)
-          </li>
-          <li>
-            At least 10 characters (and up to 100 characters)
-          </li>
-        </ul> */}
-
-        <div className='relative overflow-x-auto overflow-y-auto h-48'>
-          <table className='w-full text-sm text-center'>
-            <thead className='uppercase bg-gray-50'>
-              <tr>
-                <th className='px-6 py-3'>Task</th>
-                <th className='px-6 py-3'>Date</th>
-                <th className='px-6 py-3'>Status</th>
-                <th className='px-6 py-3'></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className='border-b'>
-                <td scope="row" className='px-6 py-4'>React js</td>
-                <td className='px-6 py-4'>2023-05-03 | 2023-05-03</td>
-                <td className='px-6 py-4'>Success</td>
-                <td className='px-6 py-4'>
-                  <button className='bg-blue-600 px-2 py-1 rounded-lg text-white'>Edit</button>
-                </td>
-              </tr>
-              <tr className='border-b'>
-                <td className='px-6 py-4 w-80'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis dolore ullam deleniti quia sint autem esse, inventore vel perferendis aliquid quod soluta quasi dolores ea, adipisci aspernatur. Eligendi, doloremque quisquam!</td>
-                <td className='px-6 py-4'>2023-05-03 | 2023-05-03</td>
-                <td className='px-6 py-4'>Success</td>
-                <td className='px-6 py-4'>
-                  <button className='bg-blue-600 px-2 py-1 rounded-lg text-white'>Edit</button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+      <div className='bg-white border border-gray-200 rounded-lg p-6'>
+        <h5 className='mb-4 text-2xl font-bold text-center'>List Tasks</h5>
+        <div>
+          <div className='relative overflow-x-auto h-full'>
+            <table className='w-full text-sm text-center'>
+              <thead className='uppercase bg-gray-50'>
+                <tr>
+                  <th className='px-6 py-3'>Task</th>
+                  <th className='px-6 py-3'>Date</th>
+                  <th className='px-6 py-3'>Status</th>
+                  <th className='px-6 py-3'></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className='border-b'>
+                  <td scope="row" className='px-6 py-4'>React js</td>
+                  <td className='px-6 py-4'>2023-05-03 | 2023-05-03</td>
+                  <td className='px-6 py-4'>Success</td>
+                  <td className='px-6 py-4'>
+                    <button className='bg-blue-600 px-2 py-1 rounded-lg text-white'>Edit</button>
+                  </td>
+                </tr>
+                <tr className='border-b'>
+                  <td className='px-6 py-4 w-80'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae labore</td>
+                  <td className='px-6 py-4'>2023-05-03 | 2023-05-03</td>
+                  <td className='px-6 py-4'>Success</td>
+                  <td className='px-6 py-4'>
+                    <button className='bg-blue-600 px-2 py-1 rounded-lg text-white'>Edit</button>
+                  </td>
+                </tr>
+                <tr className='border-b'>
+                  <td className='px-6 py-4 w-80'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae labore</td>
+                  <td className='px-6 py-4'>2023-05-03 | 2023-05-03</td>
+                  <td className='px-6 py-4'>Success</td>
+                  <td className='px-6 py-4'>
+                    <button className='bg-blue-600 px-2 py-1 rounded-lg text-white'>Edit</button>
+                  </td>
+                </tr>
+                <tr className='border-b'>
+                  <td className='px-6 py-4 w-80'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae labore</td>
+                  <td className='px-6 py-4'>2023-05-03 | 2023-05-03</td>
+                  <td className='px-6 py-4'>Success</td>
+                  <td className='px-6 py-4'>
+                    <button className='bg-blue-600 px-2 py-1 rounded-lg text-white'>Edit</button>
+                  </td>
+                </tr>
+                <tr className='border-b'>
+                  <td className='px-6 py-4 w-80'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae labore</td>
+                  <td className='px-6 py-4'>2023-05-03 | 2023-05-03</td>
+                  <td className='px-6 py-4'>Success</td>
+                  <td className='px-6 py-4'>
+                    <button className='bg-blue-600 px-2 py-1 rounded-lg text-white'>Edit</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </>
