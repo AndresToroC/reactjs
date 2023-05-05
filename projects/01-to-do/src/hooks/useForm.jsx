a0478f61fa3c7271e9c580802b11bde3e4802ff1
Andres Toro <atoro@intap.com.co>
2023-05-05 10:03:05 -0500

    Hook useForm

---
+++ projects/01-to-do/src/hooks/useForm.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+
+
+export const useForm = (initialState = {}) => {
+  const [valueForm, setvalueForm] = useState(initialState)
+
+  const handleInputChange = (e) => {
+    setvalueForm({
+      ...valueForm,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const resetForm = () => {
+    setvalueForm(initialState)
+  }
+
+  return {
+    valueForm,
+    handleInputChange,
+    resetForm
+  }
+}
