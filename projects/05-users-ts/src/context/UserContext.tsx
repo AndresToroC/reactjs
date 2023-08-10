73701ece7c20b33e33ea2c14247d79eca2320f1f
Andres Toro <54687547+AndresToroC@users.noreply.github.com>
2023-08-09 22:25:07 -0500

    Save data to localStorage

--- projects/05-users-ts/src/context/UserContext.tsx
+++ projects/05-users-ts/src/context/UserContext.tsx
@@ -10,24 +10,30 @@ interface Props {
   children: React.ReactNode
 }
 
+const dataUsers: User[] = JSON.parse(localStorage.getItem('users') || JSON.stringify(usersData))
+
 export const UserContext = createContext<UserContextType | null>(null)
 
 export const UserProvider: React.FC<Props> = ({ children }) => {
-  const [users, setUsers] = useState(usersData)
+  const [users, setUsers] = useState(dataUsers)
   const [userSelected, setUserSelected] = useState<User | null>(null)
 
   const handleDeleteUser = (uid: string) => {
     const newUsers = users.filter(user => user.uid !== uid)
 
     setUsers(newUsers)
+    localStorage.setItem('users', JSON.stringify(newUsers))
     toast.success('User deleted successfully')
   }
 
   const handleAddUser = (user: User) => {
-    setUsers([
+    const newUsers = [
       ...users,
       user
-    ])
+    ]
+
+    setUsers(newUsers)
+    localStorage.setItem('users', JSON.stringify(newUsers))
   }
 
   const handleSelectedEdit = (id: string) => {
@@ -45,9 +51,13 @@ export const UserProvider: React.FC<Props> = ({ children }) => {
   const handleUpdateUser = (user: User) => {
     const userFind = users.findIndex(userI => userI.uid === user.uid)
     
-    users[userFind] = user
-    setUsers(users)
+    const newUsers = structuredClone(users)
+    
+    newUsers[userFind] = user
+    setUsers(newUsers)
     setUserSelected(null)
+
+    localStorage.setItem('users', JSON.stringify(newUsers))
   }
   
   return (
